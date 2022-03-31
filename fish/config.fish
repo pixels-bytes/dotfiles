2f0cd30e45130b710b2b49076218cdfe89d2893a
pixels-bytes <adam@pixelsandbytes.co>
2022-03-31 15:43:34 +0000

    Fish and Alacritty Tweaks

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+starship init fish | source
+
+thefuck --alias | source
+set -g fish_user_paths "/usr/local/opt/apr/bin" $fish_user_paths
+set -g direnv_fish_mode disable_arrow
+direnv hook fish | source
+
+# Created by `pipx` on 2022-03-31 07:49:30
+set PATH $PATH /Users/abu/.local/bin
