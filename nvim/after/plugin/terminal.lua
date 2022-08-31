dccdc8280ebc2827fda51ae25dd16796807afdf7
Adam Tait <adam@pixelsandbytes.co>
2022-08-31 08:45:32 +0200

    Configure terminal

--- nvim/after/plugin/terminal.lua
+++ nvim/after/plugin/terminal.lua
@@ -14,17 +14,63 @@
 --]]
 
 
-local map = vim.keymap.set
+local status_ok, fterm = pcall(require, 'FTerm')
+if not status_ok then
+  vim.notify('Problem with FTerm')
+end
 
 local keymap = vim.keymap.set
 
+-- Setup
+local dimensions = {
+  y = 0.3,
+  height = 0.9,
+  width = 0.9
+}
+
 fterm.setup({
-  dimensions = {
-    y = 0.3,
-    height = 0.9,
-    width = 0.9,
-  },
+  dimensions = dimensions
 })
 
 keymap('n', '<C-\\>', '<CMD>lua require("FTerm").toggle()<CR>')
 keymap('t', '<C-\\>', '<c-\\><c-n><CMD>lua require("FTerm").toggle()<CR>')
+
+-- Lazygit
+local lazygit = fterm:new({
+  cmd = 'lazygit',
+  dimensions = dimensions
+})
+
+keymap('n', '<leader>g', function() lazygit:toggle() end)
+
+-- Node
+local node = fterm:new({
+  cmd = 'node',
+  dimensions = dimensions,
+})
+
+keymap('n', '<leader>tn', function() node:toggle() end)
+
+-- ncdu
+local ncdu = fterm:new({
+  cmd = 'ncdu',
+  dimensions = dimensions,
+})
+
+keymap('n', '<leader>tc', function() ncdu:toggle() end)
+
+-- htop
+local htop = fterm:new({
+  cmd = 'htop',
+  dimensions = dimensions,
+})
+
+keymap('n', '<leader>th', function() htop:toggle() end)
+
+-- Python
+local python = fterm:new({
+  cmd = 'python',
+  dimensions = dimensions,
+})
+
+keymap('n', '<leader>tp', function() node:toggle() end)
