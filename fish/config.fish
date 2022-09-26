9a33f49aa86a1b0484eb4ac3627ef1c1f266a1a5
Adam Tait <adam@pixelsandbytes.co>
2022-09-26 21:16:44 +0200

    Change to dracula theme

--- fish/config.fish
+++ fish/config.fish
@@ -2,12 +2,13 @@ starship init fish | source
 
 thefuck --alias | source
 set -g direnv_fish_mode disable_arrow
-set -gx EDITOR nvim
+set -gx EDITOR hx
 direnv hook fish | source
 
 # set theme
 # setscheme gruvbox.fish
 # setscheme catppuccin.fish
-fish_config theme choose nord
+# fish_config theme choose nord
+fish_config theme choose Dracula
 
 set PATH $PATH /Users/abu/.local/bin
