652dafc7abe8d5fe772c9e56f1b76357a35ff329
Adam Tait <adam@pixelsandbytes.co>
2022-09-26 21:17:11 +0200

    set xplr to use fish shell

--- xplr/init.lua
+++ xplr/init.lua
@@ -1,1 +1,6 @@
 version = "0.19.3"
+xplr.config.modes.builtin.action.key_bindings.on_key["!"].messages = {
+  { Call = { command = "fish", args = { "-i" } } },
+  "ExplorePwdAsync",
+  "PopMode",
+}
