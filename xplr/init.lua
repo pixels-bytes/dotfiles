70d809b7d6633688fd49b30e4209ed6fd464eefc
Adam Tait <adam@pixelsandbytes.co>
2022-09-29 10:11:55 +0200

    Add plugins to xplr installation

--- xplr/init.lua
+++ xplr/init.lua
@@ -1,6 +1,34 @@
 version = "0.19.3"
+
 xplr.config.modes.builtin.action.key_bindings.on_key["!"].messages = {
   { Call = { command = "fish", args = { "-i" } } },
   "ExplorePwdAsync",
   "PopMode",
 }
+
+local home = os.getenv("HOME")
+package.path = home
+.. "/.config/xplr/plugins/?/init.lua;"
+.. home
+.. "/.config/xplr/plugins/?.lua;"
+.. package.path
+
+require("fzf").setup()
+require("find").setup()
+require("map").setup()
+require("completion").setup()
+require"icons".setup()
+require("registers").setup()
+require("command-mode").setup()
+require("scp").setup()
+require("nuke").setup()
+require("preview-tabbed").setup()
+
+local key = xplr.config.modes.builtin.default.key_bindings.on_key
+
+key.v = {
+  help = "nuke",
+  messages = {"PopMode", {SwitchModeCustom = "nuke"}}
+}
+key["f3"] = xplr.config.modes.custom.nuke.key_bindings.on_key.v
+key["enter"] = xplr.config.modes.custom.nuke.key_bindings.on_key.o
